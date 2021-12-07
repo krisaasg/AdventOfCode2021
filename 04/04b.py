c70385b7d859d4b0a3197180ee58783724c43b4a
krisaasg <aasgaard@gmail.com>
2021-12-07 22:51:13 +0100

    Fixing 4th

--- 04/04b.py
+++ 04/04b.py
@@ -1,65 +1,87 @@
-# Not working
+# Solution works, but is a mess...
 
+winnerFound = False
 def check_tables(polledInn, boardsInn):
-    # Sjkke om table vinner og i så fall kalkulere sum.
     winner = False
     firstLoser = False
-    i = 0
-    while i < len(boardsInn) and firstLoser == False:
-        if winner == False:
-            row = 0
-            column = 0
-            tempList = []
-            for j in range(5):
-                tempRow = boardsInn[(i+j)].split()
-                tempList.append(tempRow)
-            
-            sumFound = 0
-            sumUnmarked = 0
-            
-            while row < 5:
-                column = 0
-                sumFound = 0
-                while column < 5:
-                    if tempList[row][column] in polledInn:
-                        sumFound += 1
+    save1points = 0
+    save2points = 0
+    toggle = 0
+    points = 0
+#
+    x = len(polledList)
+    while x > 0:
+        if firstLoser == False:
+            polledInn = polledInn[0:x]
+            i = 0
+            while i < len(boardsInn):
+                if winner == False and firstLoser == False:
+                    row = 0
+                    column = 0
+                    tempList = []
+                    for j in range(5):
+                        tempRow = boardsInn[(i+j)].split()
+                        tempList.append(tempRow)
+                    
+                    sumFound = 0
+                    sumUnmarked = 0
+                    
+                    while row < 5:
+                        column = 0
+                        sumFound = 0
+                        while column < 5:
+                            if tempList[row][column] in polledInn:
+                                sumFound += 1
+                            else:
+                                sumUnmarked = sumUnmarked + int(tempList[row][column])
+                            column += 1
+                        if sumFound == 5:
+                            winner = True
+                        sumFound = 0
+                        row += 1
+
+                    row = 0
+                    column = 0
+                    while column < 5:
+                        row = 0
+                        sumFound = 0
+                        while row < 5:
+                            if tempList[row][column] in polledInn:
+                                sumFound += 1
+                            row += 1
+                            if sumFound == 5:
+                                winner = True
+                        sumFound = 0
+                        column += 1
+
+                    # Storing two sets of data, since this solution will identify the last board to not win
+                    # and running through numbers in reverse order. The board we want is thus the previous 
+                    # board checked.
+                    if toggle == 0:
+                        save1points = sumUnmarked
+                        toggle = 1
                     else:
-                        sumUnmarked = sumUnmarked + int(tempList[row][column])
-                    column += 1
-                if sumFound == 5:
-                    winner = True
-                    #print("Winner")
-                sumFound = 0
-                row += 1
+                        save2points = sumUnmarked
+                        toggle = 0
+                    
+                    
+                    if winner == False and firstLoser == False:
+                        # Summere alle umerkede, gange med nummer som utløste seier (siste nummer i polledInn)
+                        points = sumUnmarked * int(polledInn[-1])
+                        firstLoser = True                       
+                        winningNumber = int(polledList[len(polledInn)])                       
+                        if toggle == 1:
+                            sumUnmarked = save1points
+                        else:
+                            sumUnmarked = save2points
+                        points = (sumUnmarked - int(winningNumber)) * int(winningNumber)
+                        print(points)
 
-            column = 0
-            while column < 5:
-                row = 0
-                while row < 5:
-                    if tempList[row][column] in polledInn:
-                        sumFound += 1
-                    row += 1
-                    if sumFound == 5:
-                        winner = True
-                        #print("Winner")
-                sumFound = 0
-                column += 1
-            
-            if winner == False and firstLoser == False:
-                # Summere alle umerkede, gange med nummer som utløste seier (siste nummer i polledInn)
-                print(sumUnmarked)
-                print(polledInn[-1])
-                points = sumUnmarked * int(polledInn[-1])
-                print("Winning value: ", points)
-                firstLoser == True
-            
+                winner = False
+                i += 6 # New board starts every 6 lines
+        x -= 1
 
-            #print(i)
-        winner = False
-        i += 6
-    return(firstLoser)
-
-f = open ("t_boards.txt", "r")
+f = open ("boards.txt", "r")
 boardsList = f.readlines()
 
 trimmedList = []
@@ -67,19 +89,7 @@ for element in boardsList:
     if element > "": # Fungerer ikke?
         trimmedList.append(element.strip())
 
-winnerFound = False
-
-polled = open ("t_polled.txt", "r")
+polled = open ("polled.txt", "r")
 polledList = polled.readline().split(",")
 
-i = len(polledList) # Starts with a list of the first five polled numbers.
-while i > 0:
-    if winnerFound == False:
-        #print (polledList[0:i])
-        winnerFound = check_tables(polledList[0:i], trimmedList)
-        #print("Winner? ", winnerFound)
-    i -= 1
-
-
-# < 120320
-# < 20560
+check_tables(polledList, trimmedList)
